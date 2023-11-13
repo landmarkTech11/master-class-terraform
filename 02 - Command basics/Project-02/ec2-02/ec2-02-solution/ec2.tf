7eb863f7a3e387a347e3c79453d104836592a5ed
Godswill012 <142279804+Godswill012@users.noreply.github.com>
2023-11-13 15:45:18 -0600

    Update ec2.tf11

--- 02 - Command basics/Project-02/ec2-02/ec2-02-solution/ec2.tf
+++ 02 - Command basics/Project-02/ec2-02/ec2-02-solution/ec2.tf
@@ -1,3 +1,37 @@
+ # Terraform Settings Block
+  terraform {
+  #required_version = "~> 1.0"
+  required_version = "~> 1.6.3"
+   required_providers {
+  aws = {
+  source = "hashicorp/aws"
+  version = "~> 3.0"
+    # Optional but recommended in production 
+  }
+    }
+  }
+
+  # Provider Block
+  provider "aws" {
+   profile = "joe"
+   region  = "us-east-2"
+  }
+
+   #Resource Block
+ resource "aws_instance" "project2ec2" {
+ ami           = "ami-089c26792dcb1fbd4"
+ instance_type = "t2.micro"
+ subnet_id     = "subnet-07128464d7032ebc1"
+ tags =  {
+   "Name"= "firstec2"
+}
+ }
+
+
+
+
+
+/*
 # Terraform Settings Block
 terraform {
   #required_version = "~> 1.0"
@@ -28,3 +62,4 @@ resource "aws_instance" "project2ec2" {
     Name = "FirstEC2"
   }
 }
+*/
